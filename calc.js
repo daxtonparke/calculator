7ffd60d8acec588af461b413218145053654d19e
daxtonparke <daxtonparke@gmail.com>
2021-11-09 16:19:05 -0700

    calculator2.0

--- calc.js
+++ calc.js
@@ -13,6 +13,8 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3]);
+
 
 
 	if (mathSymbol ==="+"){
@@ -30,9 +32,19 @@ reader.question("What would you like to calculate?", function(input){
 	else if(mathSymbol ==="sqrt"){
 		console.log(Math.sqrt(num1))
 	}
-
-	console.log(num1, mathSymbol, num2)
+	else if(mathSymbol ==="square"){
+		console.log(Math.pow(num1, 2))
+	}
+	else if(mathSymbol ==="cube"){
+		console.log(Math.pow(num1, 3))
+	}
+	else if(mathSymbol ==="power"){
+		console.log(Math.pow(num1, num2))
+	}
+	else if(mathSymbol ==="%"){
+		console.log(num1%num2)
+	}
+	// console.log(num1, mathSymbol, num2)
 	// This line closes the connection to the command line interface.
 	reader.close()
-
 });
